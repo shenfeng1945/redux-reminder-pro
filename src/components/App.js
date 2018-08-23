380afb7d673a2d86ed4287cf32da5eeee0e18ae4
shenfeng1945 <823902826@qq.com>
2018-08-23 20:29:27 +0800

    X

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,55 @@
 import React, { Component } from 'react';
+import {connect} from 'react-redux'
+import {addReminder} from '../actions'
 
 class App extends Component {
+   constructor(props){
+       super(props)
+       this.state = {
+           text:'',
+       }
+   }
+   addReminder(){
+       this.props.addReminder(this.state.text)
+   }
+   renderReminders(){
+       return (
+           <ul className="list-group col-sm-8 mt-Z">
+              <li className="list-group-item">
+                <div className="list-item">
+                   <div>text</div>
+                   <div>tiem</div>
+                </div> 
+              </li>
+           </ul>
+       )
+   }
    render(){
        return (
-           <div>hello</div>
+          <div className="App">
+            <div className="title">Reminder Pro</div> 
+            <div className="form-inline">
+               <div className="form-group">
+                 <input type="text" 
+                        className="form-control" 
+                        placeholder="I have to..."
+                        onChange={(event)=>this.setState({text:event.target.value})}
+                        />
+               </div> 
+               <button type="button"
+                       onClick={()=>this.addReminder()}
+                       className="btn btn-success">Add Reminder</button>
+            </div>
+            {this.renderReminders()}
+          </div>
        )
    } 
 }
+const mapStateToProps = (state) =>{
+    return {
+        reminders: state.reminders
+    }
+}
  
 
-export default App
+export default connect(mapStateToProps,{addReminder})(App)
--- src/index.js
+++ src/index.js
@@ -3,10 +3,24 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
+import {Provider} from 'react-redux'
+import {createStore,applyMiddleware} from 'redux'
+import rootReducer from './reducers' 
+import logger from 'redux-logger'
+import {composeWithDevTools} from 'redux-devtools-extension'
 
+
+const store = createStore(
+    rootReducer,
+    composeWithDevTools(
+      applyMiddleware(logger)
+    )
+)
 
 ReactDOM.render(
-    <App />,
+   <Provider store={store}>
+    <App />
+   </Provider>,
   document.getElementById('root')
 );
 registerServiceWorker();
