3c4980edeb72a7fc20b421166a6f499e43dcbafb
shenfeng1945 <823902826@qq.com>
2018-08-23 17:19:10 +0800

    init

--- src/index.js
+++ src/index.js
@@ -3,23 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
-import { composeWithDevTools } from 'redux-devtools-extension';
-import { Provider } from 'react-redux';
-import logger from 'redux-logger';
-import { createStore, applyMiddleware } from 'redux';
-import rootReducer from './reducers';
 
-const store = createStore(
-  rootReducer,
-  composeWithDevTools(
-    applyMiddleware(logger)
-  )
-);
 
 ReactDOM.render(
-  <Provider store={ store }>
-    <App />
-  </Provider>,
+    <App />,
   document.getElementById('root')
 );
 registerServiceWorker();
